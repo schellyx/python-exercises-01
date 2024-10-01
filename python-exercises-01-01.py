5e87ce1daf46d7f12c2779996aa55b7e2c290409
schellarie <michelleberger388@gmail.com>
2024-10-01 14:25:10 +0200

    added examples

---
+++ python-exercises-01-01.py
@@ -0,0 +1,12 @@
+# Definieren der Variablen
+
+first_name = "Michelle" # string
+last_name = "Berger" # string
+birth_year = 24 # ganzzahl
+has_license = True # boolean
+
+# Ausgabe der Variablen
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Birth Year:", birth_year)
+print("Has License:", has_license)
